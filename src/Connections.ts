16e622161060596212fa9a48f57d07e4ae99cd4e
SilentRaccoon02 <02smrnv@gmail.com>
2023-11-25 10:13:39 +0300

    Update Connections

--- src/Connections.ts
+++ src/Connections.ts
@@ -1,32 +1,45 @@
 import type DataObject from './DataObject';
 
+interface IP2P {
+    connection: RTCPeerConnection;
+    channel: RTCDataChannel;
+}
+
+type IAction = (dataObject: DataObject) => void;
+
 export default class Connections {
-    private _uuid: string | undefined;
-    private readonly _server: WebSocket;
-    private readonly _actions: any;
+    private _uuid: string | undefined = undefined;
+    private readonly _server: WebSocket = new WebSocket('ws://localhost:2512');
+    private readonly _actions = new Map<string, IAction>();
 
     public constructor () {
-        this._uuid = undefined;
-        this._server = new WebSocket('ws://localhost:2512');
         this._server.binaryType = 'arraybuffer';
-
         this._server.onopen = (event: Event) => { console.log(event); };
         this._server.onclose = (event: CloseEvent) => { console.log(event); };
         this._server.onerror = (event: Event) => { console.log(event); };
         this._server.onmessage = (event: MessageEvent) => {
             const jsonString: string = new TextDecoder().decode(event.data);
             const dataObject: DataObject = JSON.parse(jsonString);
-            this._actions[dataObject.type]?.(dataObject);
+            this._actions.get(dataObject.type)?.(dataObject);
         };
 
-        this._actions = {
-            uuid: (dataObject: DataObject) => { this._uuid = dataObject.to; }
-        };
+        this._actions.set('uuid', (dataObject: DataObject) => { this._uuid = dataObject.to; });
+        this._actions.set('nodes', (dataObject: DataObject) => {
+            dataObject.data.forEach((uuid: string) => { this.initP2P(uuid); });
+        });
     }
 
     private sendViaServer (dataObject: DataObject): void {
         const jsonString: string = JSON.stringify(dataObject);
         const bytes: Uint8Array = new TextEncoder().encode(jsonString);
         this._server.send(bytes);
+    }
+
+    private initP2P (uuid: string): void {
+        console.log(uuid);
+    }
+
+    private acceptP2P (dataObject: DataObject): void {
+
     }
 }
