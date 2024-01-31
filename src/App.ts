b366b5c9079e7540a79fb8593bd143451eea3037
SilentRaccoon02 <02smrnv@gmail.com>
2024-01-31 14:31:34 +0300

    Add array test

--- src/App.ts
+++ src/App.ts
@@ -1,23 +1,30 @@
 // @ts-expect-error emscripten
 import Module from './wasm/module'
 
-interface EmscriptenModule {
+interface ExtendedModule extends EmscriptenModule {
     cwrap: typeof cwrap
     addFunction: typeof addFunction
 }
 
 export class App {
     public constructor () {
-        Module().then((module: EmscriptenModule) => {
+        Module().then((module: ExtendedModule) => {
             this.test(module)
         })
     }
 
-    public test (module: EmscriptenModule): void {
+    private test (module: ExtendedModule): void {
         const testAdd = module.cwrap('testAdd', 'number', ['number', 'number'])
         console.log(`add: ${testAdd(20.0, 10.0)}`)
+
         const multiply = module.addFunction((a, b) => { return a * b }, 'fff')
         const testMultiply = module.cwrap('testMultiply', null, ['string'])
         testMultiply(multiply.toString())
+
+        const array = new Int8Array([2, 4, 6])
+        const buffer = module._malloc(array.length * array.BYTES_PER_ELEMENT)
+        module.HEAP8.set(array, buffer)
+        const testAccumulate = module.cwrap('testAccumulate', 'number', ['number', 'number'])
+        console.log(`accumulate: ${testAccumulate(buffer, array.length)}`)
     }
 }
